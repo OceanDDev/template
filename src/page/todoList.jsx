0a8eacc206ad9bbe92e3cbcd8c16c64018369615
Ocean <do.vinh@hitachids.com>
2025-01-09 16:35:27 +0700

    update css

--- src/page/todoList.jsx
+++ src/page/todoList.jsx
@@ -78,12 +78,12 @@ const TaskItem = ({ task, onDelete, onEdit, onToggleComplete, isEditing, setEdit
 
 const ToDoList = () => {
     const [allTasks, setAllTasks] = useState([
-        { id:"e166ea51-8620-47b0-8134-fb02cdcc4fb1", text: "Task 1", isChecked: false },
-        { id:"1aa3e3a6-878c-4c94-a8e7-13921e2487e8", text: "Task 2", isChecked: false },
-        { id:"6bb6fd58-5489-4df1-b6b8-493f31d75083", text: "Task 3", isChecked: false },
-        { id:"eb817ce4-65aa-4f8b-9fec-9242b0497421", text: "Task 4", isChecked: false },
-        { id:"c0062684-1707-4270-91ce-639e19a73d72", text: "Task 5", isChecked: false },
-        { id:"b320dac3-e9cf-45fc-8120-557774eaf824", text: "Task 6", isChecked: false },
+        { id: "e166ea51-8620-47b0-8134-fb02cdcc4fb1", text: "Task 1", isChecked: false },
+        { id: "1aa3e3a6-878c-4c94-a8e7-13921e2487e8", text: "Task 2", isChecked: false },
+        { id: "6bb6fd58-5489-4df1-b6b8-493f31d75083", text: "Task 3", isChecked: false },
+        { id: "eb817ce4-65aa-4f8b-9fec-9242b0497421", text: "Task 4", isChecked: false },
+        { id: "c0062684-1707-4270-91ce-639e19a73d72", text: "Task 5", isChecked: false },
+        { id: "b320dac3-e9cf-45fc-8120-557774eaf824", text: "Task 6", isChecked: false },
 
     ]);
     const [newTask, setNewTask] = useState("");
@@ -128,7 +128,7 @@ const ToDoList = () => {
         const confirm = window.confirm("Có xóa không ?");
         if (confirm) {
             setAllTasks(allTasks.filter((task) => !task.isChecked));
-        } 
+        }
     };
 
     const handleDeleteAllTasks = () => {
@@ -171,19 +171,19 @@ const ToDoList = () => {
             <div className="flex flex-row gap-3 justify-center mb-10">
                 <button
                     onClick={() => setFilter("all")}
-                    className="px-4 py-2 w-full bg-teal-500 text-white rounded-lg hover:bg-teal-600"
+                    className={`px-4 py-2 w-full rounded-lg ${filter === "all" ? "bg-teal-500" : "bg-teal-700"} text-white hover:bg-teal-600`}
                 >
                     ALL
                 </button>
                 <button
                     onClick={() => setFilter("done")}
-                    className="px-4 py-2 w-full bg-teal-500 text-white rounded-lg hover:bg-teal-600"
+                    className={`px-4 py-2 w-full rounded-lg ${filter === "done" ? "bg-teal-500" : "bg-teal-700"} text-white hover:bg-teal-600`}
                 >
                     DONE
                 </button>
                 <button
                     onClick={() => setFilter("todo")}
-                    className="px-4 py-2 w-full bg-teal-500 text-white rounded-lg hover:bg-teal-600"
+                    className={`px-4 py-2 w-full rounded-lg ${filter === "todo" ? "bg-teal-500" : "bg-teal-700"} text-white hover:bg-teal-600`}
                 >
                     TODO
                 </button>
